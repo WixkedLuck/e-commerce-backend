2bc3cf540fc90e076d3b7559e4a88f21c2d664b5
David Hybner <dah1998@yahoo.com>
2022-10-05 18:12:44 -0400

    Update category-routes.js
    created delete and findone routes with error handling

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -11,6 +11,27 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+
+    }
+  })
+.then(data => {
+    if (!data) {
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+    }
+    res.json(data);
+})
+.catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});
+
 });
 
 router.post('/', (req, res) => {
@@ -23,6 +44,20 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+        id: req.params.id
+    }
+})
+.then(dbdata =>{
+if(!dbdata){
+  res.status(404).json({message: "Sorry no category was found with this id"})
+}
+res.json(dbdata);
+})
+.catch(err =>{
+res.status(500).json(err);
+});
 });
 
 module.exports = router;
